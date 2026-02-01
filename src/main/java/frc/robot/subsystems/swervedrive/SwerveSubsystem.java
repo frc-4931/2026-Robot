15162fa7fecf6cc35a22525bc4d5176d62356e7b
Jakob Schoeberle <jrschoeberle@gmail.com>
2026-01-31 22:34:43 -0600

    Drive Speed Adjustment

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -89,6 +89,8 @@ public class SwerveSubsystem extends SubsystemBase
       swerveDrive = new SwerveParser(directory).createSwerveDrive(Constants.MAX_SPEED, startingPose);
       // Alternative method if you don't want to supply the conversion factor via JSON files.
       // swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed, angleConversionFactor, driveConversionFactor);
+      swerveDrive.setMaximumAttainableSpeeds((swerveDrive.getMaximumChassisVelocity() * Constants.speedMultiplier), (swerveDrive.getMaximumChassisAngularVelocity() * Constants.rotationMultiplier));
+
     } catch (Exception e)
     {
       throw new RuntimeException(e);
