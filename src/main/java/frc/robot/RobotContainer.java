0e71a6cfc891b1378f0d30dcac7f29ae187f7448
Amelia Tallman <amtallman14@yahoo.com>
2026-03-13 16:55:38 -0500

    Subsystem added for the shooter motor subsysterm into the robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -23,6 +23,7 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
+import frc.robot.subsystems. ShooterMotorSubsystem;
 import java.io.File;
 import swervelib.SwerveInputStream;
 
